849df3ec352b71a77ef2b437d6bc8ec6e34ba3e6
GarRuru <nick92251@gmail.com>
2017-04-08 18:27:01 +0800

    final ver

---
+++ VolleyBall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VolleyBall
+//
+//  Created by Yu-Hsuan Chen on 2017/4/3.
+//  Copyright © 2017年 Yu-Hsuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
